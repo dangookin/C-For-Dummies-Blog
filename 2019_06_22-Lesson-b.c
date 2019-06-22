86f5ae5f5f55500151e5c0067f58aba27a2f0760
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2019-06-22 10:59:13 -0700

    Add files via upload

---
+++ 2019_06_22-Lesson-b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char source[] = "My text will be copied";
+	char destination[] = "123456789012345678901234567890";
+
+	strncpy(destination,source,25);
+	printf("%s\n",destination);
+
+	return(0);
+}
+
