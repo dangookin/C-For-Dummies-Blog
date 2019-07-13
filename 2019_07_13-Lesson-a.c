af832fcf40a46ca51f93f3cfc1f6070d2cf2524f
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2019-07-13 09:47:53 -0700

    Add files via upload

---
+++ 2019_07_13-Lesson-a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char x[] = "nonsense";
+	char *p;
+	int a;
+
+	a = strtol(x,&p,10);
+	printf("'%s' evaluates to %d\n",x,a);
+	if( p == x )
+	{
+		printf("'%s' doesn't contain any values\n",x);
+	}
+
+	return(0);
+}
