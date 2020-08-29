c7b52bd0fcd54065a7b7c2dfb50d3ba3233b621e
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2020-08-29 09:11:41 -0700

    Add files via upload

---
+++ 2020_09_01-Exercise-skelleton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* return string representing integers 0 through 100 */
+char *verbal(int n)
+{
+}
+
+int main()
+{
+	int x;
+
+	for(x=0; x<=100; x++ )
+		printf("%s\n",verbal(x));
+
+	return(0);
+}
