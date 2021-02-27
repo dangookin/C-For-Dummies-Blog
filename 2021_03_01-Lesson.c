2b3124dce8959b5a192501ac89112aca34390a56
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2021-02-27 09:48:36 -0800

    Add files via upload

---
+++ 2021_03_01-Lesson.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int sign(int a)
+{
+}
+
+int main()
+{
+	int values[20] = { -34, 27, 0, 48, -25, 28, -55,
+		66, -17, -78, 0, -20, 40, -98, 63, -44, 59,
+		6, 65, 90
+	};
+	int x;
+
+	for( x=0; x<20; x++ )
+	{
+		printf("Sign of %3d is ", values[x] );
+		switch( sign( values[x] ))
+		{
+			case -1:
+				puts("negative");
+				break;
+			case 1:
+				puts("positive");
+				break;
+			case 0:
+				puts("zero");
+				break;
+			default:
+				puts("unknown");
+		}
+	}
+
+	return(0);
+}
