1aba9610725048078dd77e64ab0bbc4eb4206b83
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2021-05-01 09:03:52 -0700

    Add files via upload

---
+++ 2021_05-Exercise.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define begin int
+#define program main()
+#define start {
+#define print(a) puts(a);
+#define end return(0);
+#define stop }
+
+begin program
+start
+print("Hello!")
+end
+stop
