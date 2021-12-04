c299e2ae834cb21c42c74648ea8428add9bf1801
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2021-12-04 09:37:05 -0800

    Add files via upload

---
+++ 2021_12_11-Lesson-a.c
@@ -0,0 +1,26 @@
+/* eventually put the text into a file, sonnet18 */
+#include <stdio.h>
+
+int main()
+{
+	char sonnet18[] = 
+		"Shall I compare thee to a summer’s day?\n"
+		"Thou art more lovely and more temperate.\n"
+		"Rough winds do shake the darling buds of May,\n"
+		"And summer’s lease hath all too short a date.\n"
+		"Sometime too hot the eye of heaven shines,\n"
+		"And often is his gold complexion dimmed;\n"
+		"And every fair from fair sometime declines,\n"
+		"By chance, or nature’s changing course, untrimmed;\n"
+		"But thy eternal summer shall not fade,\n"
+		"Nor lose possession of that fair thou ow’st,\n"
+		"Nor shall death brag thou wand'rest in his shade,\n"
+		"When in eternal lines to Time thou grow'st.\n"
+		"  So long as men can breathe, or eyes can see,\n"
+		"  So long lives this, and this gives life to thee.\n"
+	;
+
+	printf("%s",sonnet18);
+
+	return(0);
+}
