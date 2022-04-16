9b8b857a68f6316fdf4a8fcb8815a4e85b1912a6
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2022-04-16 08:58:09 -0700

    Add files via upload

---
+++ 2022_04_23-Lesson-a.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void memory(void)
+{
+	char *p;
+
+	p = malloc(1024*1024*1024);		/* 1GB */
+	if( p==NULL )
+	{
+		fprintf(stderr,"Unable to allocate memory\n");
+		exit(1);
+	}
+}
+
+int main()
+{
+	int x;
+
+	for( x=0; x<1024; x++ )
+	{
+		printf("%4d - Allocating memory: ",x);
+		memory();
+		printf("done!\n");
+	}
+
+	return(0);
+}
