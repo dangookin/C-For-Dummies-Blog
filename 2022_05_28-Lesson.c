89d40c41bff80bb2279635670b43367ee7694b73
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2022-05-21 17:17:05 -0700

    Add files via upload

---
+++ 2022_05_28-Lesson.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *thread_funct(void *a)
+{
+	const int loop=8;
+	int x;
+
+	for( x=0; x<loop; x++ )
+	{
+		printf("*\n");
+		sleep(1);
+	}
+
+	return(NULL);
+}
+
+int main()
+{
+	char buffer[BUFSIZ];
+	int r;
+	pthread_t thd;
+
+	/* spawn the new thread */
+	r = pthread_create( &thd, NULL, thread_funct, NULL);
+	if( r!=0 )
+	{
+		perror("Thread");
+		exit(1);
+	}
+
+	/* prompt for your name */
+	printf("What is your name? ");
+	fgets(buffer,BUFSIZ,stdin);
+	printf("Hello, %s",buffer);
+
+	return(0);
+}
