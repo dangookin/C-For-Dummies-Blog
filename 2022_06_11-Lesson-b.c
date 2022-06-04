cb4873a724f3ccae64ac21f2018ea9317cb58194
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2022-06-04 09:18:34 -0700

    Add files via upload

---
+++ 2022_06_11-Lesson-b.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *thread_funct(void *a)
+{
+	const int loop=8;
+	static int x;
+
+	for( x=0; x<loop; x++ )
+	{
+		if( x==5 )
+			pthread_exit(&x);
+		printf("*\n");
+		sleep(1);
+	}
+
+	return(NULL);
+}
+
+int main()
+{
+	char buffer[BUFSIZ];
+	int r;
+	pthread_t thd;
+	void *retval;
+
+	/* spawn the new thread */
+	r = pthread_create( &thd, NULL, thread_funct, NULL);
+	if( r!=0 )
+	{
+		perror("Thread");
+		exit(1);
+	}
+	
+	/* wait for the thread */
+	r = pthread_join( thd, &retval );
+	if( r!=0 )
+	{
+		perror("Thread join");
+		exit(1);
+	}
+	printf("The thread returned value: %d\n",*((int *)retval) );
+
+	/* prompt for your name */
+	printf("What is your name? ");
+	fgets(buffer,BUFSIZ,stdin);
+	printf("Hello, %s",buffer);
+
+	return(0);
+}
