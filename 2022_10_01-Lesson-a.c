5c2b91c9eda93b3da9d9e04d7702416ffd118a21
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2022-09-24 10:20:52 -0700

    Add files via upload

---
+++ 2022_10_01-Lesson-a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	unsigned long long i = 0;
+
+	while(++i)
+		printf("%llu\n",i);
+
+	return(0);
+}
---
+++ 2022_10_01-Lesson-b.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <gmp.h>
+
+int main()
+{
+	printf("GMP version: %d\n",__GNU_MP_RELEASE);
+	return(0);
+}
