36c6da1d75def0dd6ebf050eba19cc54ea99828c
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2022-10-08 08:48:03 -0700

    Add files via upload

---
+++ 2022_10_15-Lesson-a.c
@@ -0,0 +1,22 @@
+#include <gmp.h>
+
+int main()
+{
+	mpz_t a,b,x;
+
+	/* initialize */
+	mpz_init(a);
+	mpz_init(b);
+	mpz_init(x);
+
+	/* set values */
+	mpz_set_ui(a,3000000000);	/* three billion */
+	mpz_set_ui(b,5000000000);	/* five billion */
+
+	/* add the values */
+	mpz_add(x,a,b);
+	/* output */
+	gmp_printf("%Zd + %Zd = %Zd\n",a,b,x);
+
+	return(0);
+}
