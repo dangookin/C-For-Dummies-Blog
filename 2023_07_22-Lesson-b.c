a118d9b858ebe3a6a0548b0e57731791872a24f9
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2023-07-15 10:13:22 -0700

    weekly update

---
+++ 2023_07_22-Lesson-b.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+int main()
+{
+	char *b,*s;
+
+	/* allocate/initialize buffers */
+	b = malloc( SIZE * sizeof(char) );	/* input */
+	s = malloc( sizeof(char) );			/* string */
+	if( b==NULL || s==NULL )
+	{
+		fprintf(stderr,"Memory allocation error\n");
+		exit(1);
+	}
+	/* initialize string storage */
+	*b = *s = '\0';
+
+	/* fetch input */
+	printf("Type some text: ");
+	while(1)
+	{
+		scanf("%s",b);
+		if( strcmp(b,"END")==0 )
+			break;
+		/* copy the word */
+			/* add two: space and null char */
+		s = realloc(s,strlen(s) + strlen(b) + 2);
+		if( s==NULL )
+		{
+			fprintf(stderr,"Reallocation error\n");
+			exit(1);
+		}
+		strcat(s,b);
+		strcat(s," ");
+	}
+
+	/* output results */
+	puts(s);
+
+	return(0);
+}
