90e1a3b01124ee5f4e04c8ea5e573f2b9487decf
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2023-12-09 11:26:51 -0800

    weekly update 12-9-2023

---
+++ 2023_12_16-Lesson-a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	bool b = 1;
+
+	if( b==true )
+		puts("B is true");
+	else
+		puts("B is false");
+
+	return(0);
+}
---
+++ 2023_12_16-Lesson-b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	bool b = 1;
+
+	if( b==true )
+		puts("B is true");
+	else
+		puts("B is false");
+
+	return(0);
+}
