7d498cb901163a3320849a0c644f8c04fea0c0dd
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2024-04-13 10:33:58 -0700

    Update 4-13-2024

---
+++ 2024_04_20-Lesson-a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	void a;
+
+	printf("Enter an integer: ");
+	scanf("%d",a);
+	printf("You entered %d\n",a);
+
+	return 0;
+}
---
+++ 2024_04_20-Lesson-b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	void a;
+
+	printf("Enter an integer: ");
+	scanf("%d",(int)a);
+	printf("You entered %d\n",(int)a);
+
+	return 0;
+}
