c16bb948d12e72bb1d5150243de5c6990aa66d3b
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2024-12-28 11:40:37 -0800

    Create 2025_01_04-Lesson.c

---
+++ 2025_01_04-Lesson.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+char *concatenate(int v, ...)
+{
+	va_list argc;
+	int x;
+	char *r,*temp;
+
+	va_start(argc,v);
+	for(x=0; x<v; x++)
+	{
+		temp = va_arg(argc,char *);
+		if( x==0 )
+		{
+			r = malloc( sizeof(char) * strlen(temp) +1 );
+			if( r==NULL )
+				return("NULL");
+			strcpy(r,temp);
+		}
+		else
+		{
+			r = realloc(r, sizeof(char) * strlen(temp) +1 );
+			if( r==NULL )
+				return("NULL");
+			strcat(r,temp);
+		}
+	}
+
+	/* clean up */
+	va_end(argc);
+	return(r);
+}
+
+int main()
+{
+	char *a,*b,*c;
+
+	a = concatenate(4,"alpha ","beta ","gamma ","delta");
+	printf("%s\n",r);
+
+	b = concatenate(1,"Hello, world!");
+	printf("%s\n",r);
+
+	c = concatenate(6,"f","o","o","b","a","r");
+	printf("%s\n",r);
+
+	/* clean-up */
+	free(a);
+	free(b);
+	free(c);
+	return 0;
+}
