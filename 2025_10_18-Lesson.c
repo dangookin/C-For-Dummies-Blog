53cbb5de32785618442ba5bc47a0c55ad939821d
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2025-10-11 09:24:06 -0700

    Create 2025_10_18-Lesson.c

---
+++ 2025_10_18-Lesson.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* allocate storage for ten integers */
+int *ten(void)
+{
+	int *iptr = NULL;
+
+	iptr = malloc( sizeof(int) * 10 );
+
+	return(iptr);
+}
+
+int main()
+{
+	int **tenint = NULL;
+	int y;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	/* allocate 10 sticks of 10 integers */
+	for( y=0; y<10; y++ )
+	{
+		/* allocate first stick */
+		if( tenint==NULL )
+		{
+			tenint = malloc( sizeof( int *) );
+			if( tenint==NULL )
+			{
+				fprintf(stderr,"Unable to allocate storage\n");
+				exit(1);
+			}
+		}
+		else
+		/* allocate additional sticks */
+		{
+			tenint = realloc(tenint, sizeof( int *) * (y+1) );
+			if( tenint==NULL )
+			{
+				fprintf(stderr,"Unable to reallocate storage\n");
+				exit(1);
+			}
+		}
+		/* get the storage */
+		*(tenint+y) = ten();
+		if( *(tenint+y)==NULL )
+		{
+			fprintf(stderr,"Unable to allocate stick %d\n",y);
+			exit(1);
+		}
+		printf("Stick %d allocated\n",y);
+	}
+
+	/* clean-up */
+	for( y=0; y<10; y++ )
+		free( *(tenint+y) );
+	free(tenint);
+	puts("All sticks freed");
+	return 0;
+}
