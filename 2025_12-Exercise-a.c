9d01282d3ccad8c1566bcd9d8546f8eaa1ebf22c
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2025-12-06 10:00:12 -0800

    20251206 updates

---
+++ 2025_12-Exercise-a.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int greatest(int a, int b, int c)
+{
+	int max;
+
+	max = a;
+	if( b>max )
+		max = b;
+	if( c>max )
+		max = c;
+
+	return(max);
+}
+
+int main()
+{
+	int x,y,z,g;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	/* generate three random values from 2 to 25 */
+	x = rand() % 24 + 2;
+	y = rand() % 24 + 2;
+	z = rand() % 24 + 2;
+
+	g = greatest(x,y,z);
+	printf("The greatest of %d, %d, and %d is %d\n",
+			x,y,z,g
+		  );
+
+	return 0;
+}
---
+++ 2025_12_13-Lesson-b.c
@@ -0,0 +1,26 @@
+#include <locale.h>
+#include <wchar.h>
+
+#define RED "\e[31m"
+#define NORMAL "\e[m"
+
+int main()
+{
+	wchar_t chw;
+
+	/* set the locale for wide characters */
+	setlocale(LC_ALL,"");
+
+	for( chw=0; chw<=127; chw++ )
+	{
+		wprintf(L"%02X %03d ",chw,chw);
+		if( chw<32 )
+			wprintf(L"%s%lc%s",RED,chw+9216,NORMAL);
+		else if( chw==127 )
+			wprintf(L"%s%lc%s",RED,9249,NORMAL);
+		else
+			putwchar(chw);
+		putwchar(L'\n');
+	}
+	return 0;
+}
