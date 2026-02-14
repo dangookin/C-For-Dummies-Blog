a6adc1bb7b27e1b00e427ee9cf34d799a01e5b34
Dan Gookin <47089523+dangookin@users.noreply.github.com>
2026-02-14 12:02:27 -0800

    Create 2026_02_21-Lesson.c

---
+++ 2026_02_21-Lesson.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define mouse_enable() printf("\e[?1000h")
+#define mouse_extended() printf("\e[?1006h")
+#define mouse_motion() printf("\e[?1003h")
+#define mouse_disable() printf("\e[?1000l")
+
+struct mouse_data {
+	int button;
+	int column;
+	int row;
+};
+
+/* read an integer from a string
+   Exercise 2026_02 */
+char *extract(char *s)
+{
+	static char *sp;
+
+	/* check for recall */
+	if( s != NULL )
+		sp = s;
+	else
+		while( isdigit(*sp) )
+			sp++;
+
+	/* find the next digit */
+	while( *sp != '\0' )
+	{
+		if( isdigit(*sp) )
+		{
+			return(sp);
+		}
+		sp++;
+	}
+
+	return(NULL);
+}
+
+/* obtain values from mouse data */
+void read_mouse(char *b, struct mouse_data *m)
+{
+	m->button = atoi(extract(b));
+	m->column = atoi(extract(NULL));
+	m->row = atoi(extract(NULL));
+}
+
+int main()
+{
+	struct termios original,noecho;
+	struct mouse_data mickey;
+	char buffer[13];
+	int key;
+
+	/* obtain terminal configuration */
+	tcgetattr(fileno(stdin),&original);
+	noecho = original;		/* duplicate */
+		/* enable raw input */ 
+	noecho.c_lflag = noecho.c_lflag ^ ICANON;
+		/* disable full duplex */
+	noecho.c_lflag = noecho.c_lflag ^ ECHO;
+		/* update terminal definition */
+	tcsetattr(fileno(stdin), TCSANOW, &noecho);
+
+	/* enable mouse reporting */
+	mouse_enable();
+	mouse_motion();
+	mouse_extended();
+	/* remove line buffering */
+	setvbuf(stdin,NULL,_IONBF,0);
+
+	/* read stdin */
+	puts("Play with the mouse; press Enter to end");
+	while( (key=getchar()) != '\n' )
+	{
+		read(fileno(stdin),buffer,12);
+		buffer[12] = '\0';		/* make it a string */
+		read_mouse(buffer,&mickey);
+		printf("Button %2d Column %2d Row %2d\n",
+				mickey.button,
+				mickey.column,
+				mickey.row
+			  );
+	}
+
+	/* clean-up */
+	mouse_disable();	/* disable mouse reporting */
+	tcsetattr(fileno(stdin), TCSANOW, &original);
+	return 0;
+}
